cc3d1e1c83f552b020ab50ed02d3ac4064c64064
Jeziel Almeida <jeziel.almeida16@gmail.com>
2023-11-07 23:22:07 -0300

    Add scrollend event listener to mobile pagination

--- src/components/mobilePagination/MobilePagination.tsx
+++ src/components/mobilePagination/MobilePagination.tsx
@@ -16,6 +16,39 @@ const MobilePagination = ({ carousel, carsLength }: MobilePaginationProps) => {
             carousel.current.scrollLeft = actualBtn * 250;
         }
     }
+
+    carousel.current?.addEventListener('scrollend', () => {
+        const scrollNumber = carousel.current?.scrollLeft;
+        console.log(scrollNumber);
+
+        if(scrollNumber) {
+            if(scrollNumber < 80) carousel.current.scrollLeft = 0;
+            if(scrollNumber >= 80 && scrollNumber < 330) {
+                carousel.current.scrollLeft = 250;
+            }
+            if(scrollNumber >= 330 && scrollNumber < 580) {
+                carousel.current.scrollLeft = 500;
+            }
+            if(scrollNumber >= 580 && scrollNumber < 830) {
+                carousel.current.scrollLeft = 750;
+            }
+            if(scrollNumber >= 830 && scrollNumber < 1080) {
+                carousel.current.scrollLeft = 1000;
+            }
+            if(scrollNumber >= 1080 && scrollNumber < 1330) {
+                carousel.current.scrollLeft = 1250;
+            }
+            if(scrollNumber >= 1330 && scrollNumber < 1580) {
+                carousel.current.scrollLeft = 1500;
+            }
+            if(scrollNumber >= 1580 && scrollNumber < 1830) {
+                carousel.current.scrollLeft = 1750;
+            }
+            if(scrollNumber >= 1830 && scrollNumber < 2080) {
+                carousel.current.scrollLeft = 2000;
+            }
+        }
+    })
     
     return (
         <div className={styles.mobilePagination}>
